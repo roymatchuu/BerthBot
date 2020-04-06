f844cd588079d26694a69865ed25a700858246ca
Roy Matthew Ramos <roymr@uci.edu>
2020-04-06 11:24:41 -0700

    Fixed some bugs with daily checker

--- berthBot.py
+++ berthBot.py
@@ -14,31 +14,38 @@ client.remove_command('help')
 
 async def daily_check():
     berthFlag = False
-    greeting_time = '15:11'
+    greeting_time = '12:00'
     tempBDAY = ' '
 
     await client.wait_until_ready()
     # print("I AM READY!")
-    msg_channel = client.get_channel(123456)
+    msg_channel = client.get_channel(12345)
 
     bday_bucket = set()
-    
     while not client.is_closed():
         # checkFile = open(r"path","r")
-        checkFile = open(r"berthList1.txt","r")
+        now = datetime.now()
+        date_today = now.strftime('%m/%d')
+
+
+        # print("Top of while loop ")
+        # print(f"Today's date: {date_today}")
+        checkFile = open(r"berthList_win.txt","r")
         checkLine = checkFile.readline()
+
+        
 
         if(checkLine != None):
             while checkLine:
                 tempCL = checkLine.split(" ")
-                now = datetime.now()
+                # now = datetime.now()
 
                 CL = tempCL[-2]
 
-                date_today = now.strftime('%m/%d')
+                # date_today = now.strftime('%m/%d')
                 # print(f'tempCL[1]:{CL} now:{date_today}')
                 if(CL == date_today):
-                    # print('Found equal dates!')
+                    print(f'Found equal dates on {date_today}')
                     tempBDAY = tempCL[-1]
                     berthFlag = True
                     bday_bucket.add(tempBDAY)
@@ -49,10 +56,10 @@ async def daily_check():
         checkFile.close()
 
         time_now = datetime.strftime(datetime.now(),'%H:%M')
-        print(f'greeting time:{greeting_time} time_now:{time_now}')
+        # print(f'greeting time:{greeting_time} time_now:{time_now}')
         
         if(greeting_time == time_now and berthFlag == True):
-            print("it is time!")
+            print("it is time for a birthday greeting!")
             for bdays in bday_bucket:
                 await msg_channel.send(f"@everyone, today is someone's special day.")
                 await msg_channel.send("-----------:balloon::tada::birthday::partying_face: -----------") 
@@ -60,10 +67,12 @@ async def daily_check():
                 await msg_channel.send("-----------:balloon::tada::birthday::partying_face: -----------")
                 print("birthday greeting sent!")
             bday_bucket.clear()
-
+            
+            berthFlag = False
             time = 84600
         else: 
             time = 15   
+        
         await asyncio.sleep(time)
 
 # Sets Berth's status to online and the status message 
@@ -137,7 +146,7 @@ async def add(ctx, member, date):
         # addFile stores the opened file, path will vary 
         # BerthBot runs on a raspberry pi so path will follow a linux convention
         # addFile = open('filepath',"r+")
-        addFile = open(r"berthList1.txt", "r+")
+        addFile = open(r"berthList_win.txt", "r+")
 
         # reads each line and modifies the 'buff' variable if the user already has an inputted birthdate
         line = addFile.readline()
@@ -154,8 +163,8 @@ async def add(ctx, member, date):
 
         if(month < 10 and len(tempDate[0]) != 2):
             bObj.bday = '0' + bObj.bday
-        if(day < 10 and len(tempDate[0]) != 2):
-            temp = bObj.bdayx[0:3]
+        if(day < 10 and len(tempDate[1]) != 2):
+            temp = bObj.bday[0:3]
             bObj.bday = temp + "0" + bObj.bday[-1]
 
         # validity checks and makes sure that the dates are valid and that here aren't any duplicate users with birthdays
